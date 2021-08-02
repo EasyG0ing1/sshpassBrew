51ebbd0c04dea9f403189aca98f04ce71cd61762
Michael Sims <EasyG0ing1@users.noreply.github.com>
2021-08-02 11:59:51 -0700

    Update sshpass.rb

--- Formula/sshpass.rb
+++ Formula/sshpass.rb
@@ -1,9 +1,13 @@
 require 'formula'
 
 class Sshpass < Formula
-  url 'http://sourceforge.net/projects/sshpass/files/sshpass/1.09/sshpass-1.09.tar.gz'
-  homepage 'http://sourceforge.net/projects/sshpass'
-  sha256 '71746e5e057ffe9b00b44ac40453bf47091930cba96bbea8dc48717dedc49fb7'
+  desc "Non-interactive ssh password auth"
+  homepage "http://sourceforge.net/projects/sshpass/"
+  url "https://raw.githubusercontent.com/EasyG0ing1/sshpassBrew/main/Formula/sshpass.rb"
+  name "sshpass"
+  version "1.0.9"
+  sha256 "71746e5e057ffe9b00b44ac40453bf47091930cba96bbea8dc48717dedc49fb7"
+  license "GPL-2.0-only"
 
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
